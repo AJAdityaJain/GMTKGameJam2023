d04a380afc2a5d6b41b366e2c6d03e1928f1ce00
BetaFruit <113561469+betafruit@users.noreply.github.com>
2023-07-08 12:23:08 -0400

    fix particles please

--- Assets/Scripts/ClickableBehaviour.cs
+++ Assets/Scripts/ClickableBehaviour.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public abstract class ClickableBehaviour : MonoBehaviour
 {
+    private GameObject clickparticle;
+
+    private void Update()
+    {
+        clickparticle = GameObject.Find("ClickParticle");
+        if (clickparticle == null)
+            Debug.LogError("ClickParticle not found");
+    }
+
     private void OnMouseDown()
     {
         transform.parent.GetComponent<ClickManager>().active = this;
+        clickparticle.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        clickparticle.GetComponent<ParticleSystem>().Play();
     }
 
     public abstract void Left(float mag);
