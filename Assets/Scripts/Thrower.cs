8ce8b7be69f8fb1dc9d07412693edf12fa1d6618
Aditya Jain <adityajain121108@gmail.com>
2023-07-09 21:30:43 +0530

    6

--- Assets/Scripts/Thrower.cs
+++ Assets/Scripts/Thrower.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class Thrower : MonoBehaviour
 {
+    Animator anim;
     public Vector2 Direction = Vector2.up;
     public float Magnitude = 10f;
     public float SecondsDelay = 2f;
     public GameObject prefab;
+    public Transform BallTransform;
     public float instanceamount;
 
     public float time = 0f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         time += Time.deltaTime;
         if (time >= SecondsDelay)
         {
+
             time = 0f;
-            var go = Instantiate(prefab, transform.position, Quaternion.Euler(0f, 0, Random.Range(0, 360)));
-            go.transform.parent = gameObject.transform;
-            go.GetComponent<Rigidbody2D>().velocity = Direction.normalized*Magnitude;
-            go.name = "paperball " + instanceamount.ToString();
-            instanceamount += 1;
+            anim.Play("Jeff_Throw");
+            StartCoroutine(UpdateCOur());
         }
+    }
+
+    private IEnumerator UpdateCOur()
+    {
+
+        yield return new WaitForSeconds(0.2f);
+        var go = Instantiate(prefab, BallTransform.position, Quaternion.Euler(0f, 0, Random.Range(0, 360)));
+        go.transform.parent = gameObject.transform;
+        go.GetComponent<Rigidbody2D>().velocity = Direction.normalized * Magnitude;
+        go.name = "paperball " + instanceamount.ToString();
+        instanceamount += 1;
+
     }
 }
